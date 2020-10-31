fb61a441bf90cfacc0f6dc6e5f250087f379adc0
JJ <jeongjae1996@naver.com>
2020-10-31 22:26:08 +0900

    first upload
    프로그래머스 고득점 Kit 정렬 K번째수 (level 1) 업로드

---
+++ Programmers/코딩테스트 고득점 Kit/level 1/정렬/K번째수/K번째수.py
@@ -0,0 +1,9 @@
+def solution(array, commands):
+    answer = []
+    
+    for c in commands:
+        s_list = array[c[0] - 1 : c[1]]
+        s_list.sort()
+        answer.append(s_list[c[2]-1])
+    
+    return answer
