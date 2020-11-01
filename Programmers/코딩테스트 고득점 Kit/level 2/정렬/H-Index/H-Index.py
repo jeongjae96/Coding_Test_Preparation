9e88f709d8b1c81c54120500518564419bb71037
JJ <jeongjae1996@naver.com>
2020-11-01 16:45:09 +0900

    minor changes
    minor changes

--- Programmers/코딩테스트 고득점 Kit/level 2/정렬/H-Index/H-Index.py
+++ Programmers/코딩테스트 고득점 Kit/level 2/정렬/H-Index/H-Index.py
@@ -9,11 +9,11 @@ def solution(citations):
     citations.sort()
     
     # 논문 n편 중, 인용된 횟수가 h편 이상 또는 이하여야 하므로, H-Index는 0 이상 n 이하.
-    for i in range(len(citations)+1): 
-        above = sum(j >= i for j in citations) # i번 이상 인용된 논문 수
-        below = sum(j <= i for j in citations) # i번 이하 인용된 논문 수
+    for h in range(len(citations)+1): 
+        above = sum(i >= h for i in citations) # h번 이상 인용된 논문 수
+        below = sum(i <= h for i in citations) # h번 이하 인용된 논문 수
         
-        if above >= i and below <= i:
-            answer = i
+        if above >= h and below <= h:
+            answer = h
     
     return answer
--- Programmers/코딩테스트 고득점 Kit/level 2/정렬/가장 큰 수/가장 큰 수.py
+++ Programmers/코딩테스트 고득점 Kit/level 2/정렬/가장 큰 수/가장 큰 수.py
@@ -8,6 +8,6 @@ def solution(numbers):
     # x*3인 이유는 x*2일 경우 9, 991 같은 경우 991이 더 앞편에 정렬되기 때문
     
     answer = str(int(''.join(numbers)))
-    # int로 변환하는 이유는 numbers가 ['0', '0', '0', '0']일 경우 '0000'을 0으로 변      환하기 위해
+    # int로 변환하는 이유는 numbers가 ['0', '0', '0', '0']일 경우 '0000'을 0으로 변환하기 위해
     
     return answer
