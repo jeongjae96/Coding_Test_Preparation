9a966d31f931e9c4bcb56a6db08998d58f743b82
JJ <jeongjae1996@naver.com>
2021-12-11 22:28:39 +0900

    modified

--- 이것이 취업을 위한 코딩 테스트다 with 파이썬/다이나믹 프로그래밍/피보나치_수열_반복적.py
+++ 이것이 취업을 위한 코딩 테스트다 with 파이썬/다이나믹 프로그래밍/피보나치_수열_반복적.py
@@ -1,3 +1,11 @@
+'''
+top-down/memoization/하향식
+bottom-up/상향식
+
+DP 테이블: bottom-up 방식에서 사용되는 결과 저장용 리스트
+memoization은 top-down에 국한되어 사용되는 표현. 엄밀히 말하면 메모이제이션은 이전에 계산된 결과를 일시적으로 기록해 놓는 넓은 개념을 의미. DP와는 별도의 개념.
+'''
+
 d = [0] * 100
 
 d[1] = 1
